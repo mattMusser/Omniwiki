59250e2fb0294e233e9e74dafe8cbc359a70a1b1
mattMusser <webdevelopmentlyspeaking@gmail.com>
2017-12-12 16:35:41 -0600

    Trying to fix connection refused error

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -53,17 +53,21 @@ Rails.application.configure do
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 
   # Default url configuration for Devise
-  config.action_mailer.default_url_options = { host: 'localhost'}
-  #config.action_mailer.delivery_method = :smtp
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000}
+  config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.default :charset => "utf-8"
+
+  config.action_mailer.default_url_options = { :host => 'omnipedia.herokuapp.com' }
+  Rails.application.routes.default_url_options[:host] = 'omnipedia.herokuapp.com'
 
   config.action_mailer.smtp_settings = {
-    address: “smtp.gmail.com”,
+    address: "smtp.gmail.com",
     port: 587,
-    domain: ENV[“GMAIL_DOMAIN”],
-    authentication: “plain”,
+    authentication: "plain",
     enable_starttls_auto: true,
-    user_name: ENV[“GMAIL_USERNAME”],
-    password: ENV[“GMAIL_PASSWORD”]
-    }
+    user_name: ENV["GMAIL_USERNAME"],
+    password: ENV["GMAIL_PASSWORD"]
+  }
 end
