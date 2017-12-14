d02ea00018e5342e1aa33f3772391ec47dd53f29
mattMusser <webdevelopmentlyspeaking@gmail.com>
2017-12-14 10:52:44 -0600

    Still trying to get heroku app working

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -90,12 +90,13 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   # Default url configuration for Devise
-  config.action_mailer.default_url_options = { :host => 'omnipedia.herokuapp.com'
+  config.action_mailer.default_url_options = { :host => 'omnipedia.herokuapp.com' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = false
   config.action_mailer.default :charset => "utf-8"
 
+  config.action_mailer.default_url_options = { :host => 'omnipedia.herokuapp.com' }
   Rails.application.routes.default_url_options[:host] = 'omnipedia.herokuapp.com'
 
   config.action_mailer.smtp_settings = {
